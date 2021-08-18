5ca089d533078a4f6be3e9f3b2d4e71c15de3268
Himmel König <itogreniyorum@gmail.com>
2021-08-18 22:34:13 +0300

    card changed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,5 @@
 import React from "react";
 const Card = ({ item }) => {
-  // {
-  //     "char_id": 114,
-  //     "name": "Charles McGill",
-  //     "birthday": "Unknown",
-  //     "occupation": [
-  //         "Lawyer"
-  //     ],
-  //     "img": "https://vignette.wikia.nocookie.net/breakingbad/images/3/3e/BCS_S3_ChuckMcGill.jpg/revision/latest?cb=20170327185308",
-  //     "status": "Deceased",
-  //     "nickname": "Chuck",
-  //     "appearance": null,
-  //     "portrayed": "Michael McKean",
-  //     "category": "Better Call Saul",
-  //     "better_call_saul_appearance": [
-  //         1,
-  //         2,
-  //         3,
-  //         4
-  //     ]
-  // }
   return (
     <div className="card">
       <img src={item.img} alt="img" />
