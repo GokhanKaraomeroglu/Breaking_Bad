04257781f997b4b0783621f805df5871d32ca40b
Gökhan Karaömeroğlu <80010235+GokhanKaraomeroglu@users.noreply.github.com>
2022-08-18 20:21:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,13 +18,12 @@ function App(){
 
   return(
     <div className = 'App'>
-      <header className = 'header'>
-      <img 
-      src = "https://www.logolynx.com/images/logolynx/55/55a537ca9549600cf7d1d0b274b3bc61.png"
-      alt = 'Breaking Bad Logo'
-      />
-      </header>
-
+      <div className = 'header'>
+        <img 
+          src = "https://www.logolynx.com/images/logolynx/55/55a537ca9549600cf7d1d0b274b3bc61.png"
+          alt = 'Breaking Bad Logo'
+         />
+      </div>
       <div className = 'cards'>
         {itemList.map((item)=> (
           <Card key= {item.char_id} item={item} />
